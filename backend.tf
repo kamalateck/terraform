6ef404a07c8ebc6eed8da11bd84b24ef075de663
kamalateck <kamalateck.da@gmail.com>
2026-04-24 16:14:09 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "storagestatefile"  # GCS bucket name
+    bucket = "storagestate_file"  # GCS bucket name
     prefix = "terraform/state"              # Path within the bucket (e.g., a folder structure)
   }
 }
