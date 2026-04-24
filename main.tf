6c4a8a1fc76fb2990798a6c4117be3af7b60a9e5
kamalateck <kamalateck.da@gmail.com>
2026-04-24 16:14:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,4 @@ resource "google_compute_subnetwork" "subnet" {
   ip_cidr_range = "10.0.0.0/24"
 }
 
-resource "google_storage_bucket" "importbucket07" {
-  name = "importbucket07"
-  location = "US"
-}
+ 
