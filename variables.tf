d47195103a99244ddcc73465ecb8a798d1ca0bf6
kamalateck <kamalateck.da@gmail.com>
2026-04-24 10:17:40 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,24 @@
 variable "instance_count" {
+  type    = number
   default = 2
 }
 
 variable "instance_name" {
+  type    = string
   default = "my-instance"
 }
 
 variable "instance_machine_type" {
+  type    = string
   default = "e2-medium"
 }
 
 variable "region" {
+  type    = string
   default = "us-central1"
 }
 
 variable "zone" {
+  type    = string
   default = "us-central1-a"
 }
